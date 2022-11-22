c83125121c34a5d01a113f7801c5ac15d1a90ae1
liyuansong <yuansong.l@aistarfish.com>
2022-11-22 13:47:57 +0800

    初始化项目

---
+++ src/main/java/com/lys/aapay/AAPayMain.java
@@ -0,0 +1,12 @@
+package
+        com.lys.aapay;
+
+/**
+ * @author liyunsong
+ * Created by on 2022-11-22 13:47
+ */
+public class AAPayMain {
+
+
+
+}
---
+++ src/main/java/com/lys/aapay/AaPayApplication.java
@@ -0,0 +1,13 @@
+package com.lys.aapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AaPayApplication.class, args);
+    }
+
+}
